d0631a49dcd8f3f367b7a00f09d8863e6b898b86
nurelzt <112380423+nurelzt@users.noreply.github.com>
2025-11-11 03:08:52 +0700

    finalll
    Prevent duplicate events for touch and click interactions.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const envelope = document.querySelector('.envelope');
 
-function toggleEnvelope() {
+function toggleEnvelope(e) {
+  // mencegah event ganda untuk touch & click
+  if (e.type === 'touchstart') {
+    e.preventDefault();
+  }
   envelope.classList.toggle('open');
 }
 
